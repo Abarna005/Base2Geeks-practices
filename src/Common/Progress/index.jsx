432a11c086ecca1725567ff369abb1fe288ee6fa
Abarna005 <abarnabar33@gmail.com>
2023-09-08 17:49:00 +0530

    created progress

---
+++ src/Common/Progress/index.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { LinearProgress } from "@mui/material/LinearProgress";
+
+export default function Progress() {
+  return (
+    <div>
+      <LinearProgress variant="buffer" value={Progress} valueBuffer={buffer} />
+      <h1>hii</h1>
+    </div>
+  );
+}
